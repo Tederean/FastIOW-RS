d05564bbc3e6af7d5a637e522dd50391438019eb
Tederean <tederean@gmail.com>
2024-05-06 21:04:55 +0200

    Update

---
+++ iowarrior-embedded-hal/src/communication/iowkit/mod.rs
@@ -0,0 +1,5 @@
+mod communication_data;
+pub(crate) mod communication_service;
+pub(crate) mod initialization_service;
+
+pub use self::communication_data::*;
---
+++ iowarrior-embedded-hal/src/communication/usbhid/mod.rs
@@ -0,0 +1,6 @@
+mod communication_data;
+pub(crate) mod communication_service;
+pub(crate) mod initialization_service;
+mod revision_service;
+
+pub use self::communication_data::*;
